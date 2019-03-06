42bdd52ab2c0bf3102f4076d082515db45ef5350
bentonsmith <bdsmith98@gmail.com>
2019-03-06 12:53:14 -0700

    Added usage message

--- src/driver.py
+++ src/driver.py
@@ -2,9 +2,12 @@ import sys
 from cols import removeNames, removeColumns
 from aggregate import removeAggregate
 from scramble import addErrors
+from usage import usage
 
 
-if sys.argv[1] == "removeAggregate":
+if len(sys.argv) <= 1:
+    usage()
+elif sys.argv[1] == "removeAggregate":
     removeAggregate(sys.argv[2])
 elif sys.argv[1] == "prepare":
     removeNames(sys.argv[2])
@@ -12,3 +15,5 @@ elif sys.argv[1] == "removeColumns":
     removeColumns(sys.argv[2])
 elif sys.argv[1] == "addErrors":
     addErrors(sys.argv[2])
+else:
+    usage()
---
+++ src/usage.py
@@ -0,0 +1,3 @@
+def usage():
+    print("Usage: filename command")
+    print("Command options:\n\t-removeAggregates\n\t-prepare\n\t-removeColumns\n\t-addErrors")
